fe58ee30b400d62fa0a5d7a4541b36c990baa693
Adrian Rumpold <a.rumpold@gmail.com>
2015-08-04 15:56:02 +0200

    Removed model classes, since they will be generated through Texo

--- src/main/java/model/datatypes/AggregationType.java
+++
@@ -1,7 +0,0 @@
-package model.datatypes;
-
-public enum AggregationType {
-    None,
-    Aggregation,
-    Composition
-}
--- src/main/java/model/datatypes/ParameterDirection.java
+++
@@ -1,7 +0,0 @@
-package model.datatypes;
-
-public enum ParameterDirection {
-    in,
-    out,
-    inout
-}
