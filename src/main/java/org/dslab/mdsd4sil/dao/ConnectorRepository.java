835f3f780dbe587fc6509e415c71d4c41b42aa50
Adrian Rumpold <a.rumpold@gmail.com>
2015-09-16 14:36:07 +0200

    Connector repository supports fetching incoming and outgoing connectors for model entities

--- src/main/java/org/dslab/mdsd4sil/dao/ConnectorRepository.java
+++ src/main/java/org/dslab/mdsd4sil/dao/ConnectorRepository.java
@@ -1,9 +1,38 @@
 package org.dslab.mdsd4sil.dao;
 
 import org.dslab.mdsd4sil.metamodel.emf.enterprisearchitect.Connector;
+import org.dslab.mdsd4sil.metamodel.emf.enterprisearchitect.ModelEntity;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Created by Adriano on 19.08.2015.
  */
 public final class ConnectorRepository extends GenericJdbcRepository<Connector, Integer> {
+
+    public List<Connector> getIncomingConnectors(ModelEntity entity) {
+        final String query = getEntityMapper().getSelectQuery() + "\nAND end_object_id = ?";
+        try (final PreparedStatement sql = jdbc.prepareStatement(query)) {
+            sql.setInt(1, entity.getObjectId());
+
+            return executeStatement(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Connector> getOutgoingConnectors(ModelEntity entity) {
+        final String query = getEntityMapper().getSelectQuery() + "\nAND start_object_id = ?";
+        try (final PreparedStatement sql = jdbc.prepareStatement(query)) {
+            sql.setInt(1, entity.getObjectId());
+
+            return executeStatement(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
