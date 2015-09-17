06f2b18f0e78ba3f255d3ce53262d9934fa745e0
Adrian Rumpold <a.rumpold@gmail.com>
2015-09-17 13:31:34 +0200

    ModelEntityRepository can fetch objects by diagram

--- src/main/java/org/dslab/mdsd4sil/dao/ModelEntityRepository.java
+++ src/main/java/org/dslab/mdsd4sil/dao/ModelEntityRepository.java
@@ -1,7 +1,9 @@
 package org.dslab.mdsd4sil.dao;
 
-import org.dslab.mdsd4sil.metamodel.emf.enterprisearchitect.Connector;
+import org.dslab.mdsd4sil.metamodel.emf.enterprisearchitect.Diagram;
 import org.dslab.mdsd4sil.metamodel.emf.enterprisearchitect.ModelEntity;
+import org.dslab.mdsd4sil.util.database.MapperRegistry;
+import org.dslab.mdsd4sil.util.database.ResultSetMapper;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -23,7 +25,22 @@ public final class ModelEntityRepository extends GenericJdbcRepository<ModelEnti
         }
     }
 
-    public List<Connector> findConnectorsByStereotype(String stereotype) {
-        return null;
+    public List<ModelEntity> findByDiagram(Diagram d) {
+        final ResultSetMapper<Diagram> diagramMapper = MapperRegistry.INSTANCE.getMapper(Diagram.class);
+        final ResultSetMapper<ModelEntity> mapper = getEntityMapper();
+        final String query = String.format("SELECT %s FROM %s entity JOIN t_diagramobjects dobj ON entity.%s = dobj.object_id JOIN %s diagram ON dobj.diagram_id = diagram.%s WHERE diagram.%s = ?",
+                mapper.getColumnList("entity"),
+                mapper.getTableName(), mapper.getIdColumn(),
+                diagramMapper.getTableName(), diagramMapper.getIdColumn(),
+                diagramMapper.getIdColumn());
+
+        try (final PreparedStatement sql = jdbc.prepareStatement(query)) {
+            sql.setObject(1, d.getDiagramId());
+
+            return executeStatement(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
